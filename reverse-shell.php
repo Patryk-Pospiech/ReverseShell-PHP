e059b39f40c371d2b0ba1ae662c121b6deb4fd34
Patryk-Pospiech <153620007+Patryk-Pospiech@users.noreply.github.com>
2023-12-12 14:12:26 +0000

    reverse-shell.php

--- reverse-shell.php
+++ reverse-shell.php
@@ -1,4 +1,8 @@
 //made and published by Patryk.P 
+//This tool may be used for legal purposes only.  Users take full responsibility
+// for any actions performed using this tool.  The author accepts no liability
+// for damage caused by this tool.  If these terms are not acceptable to you, then
+// do not use this tool.
 <?php
 
 set_time_limit (0);
